640131980e6755c857eb952d44235e9847b881fa
Kamil Babik <Kamil.Babik@microsoft.wsei.edu.pl>
2021-10-16 10:53:29 +0200

    second commit

---
+++ Apka/Apka/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Apka
+{
+    class Program
+    {
+        static void checkPrime(int podana)
+        {
+            int counter = 0;
+            for(int i = 1; i<podana; i++)
+            {
+                if (podana % i == 0)
+                {
+                    Console.WriteLine()
+                }
+            }
+            if (counter > 2)
+            {
+                Console.WriteLine("Nie jest liczbą pierwszą");
+            }
+            else
+            {
+                Console.WriteLine("Jest liczbą pierwszą");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            int podana = Convert.ToInt32(Console.ReadLine());
+            checkPrime(podana);
+
+        }
+    }
+}
